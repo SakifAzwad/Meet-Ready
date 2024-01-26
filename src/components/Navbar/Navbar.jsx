53a5cab16946ec5fee00468f3566aac686c30312
SakifAzwad <sakifazwad18@gmail.com>
2024-01-26 21:18:15 +0600

    bug fixed2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = ()=> {
             <li>
               <Link
                 className="font-normal relative after:bg-purple-500 after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-500 cursor-pointer"
-                href="/mybookings"
+                href="/pricing"
               >
                 Pricing
               </Link>
