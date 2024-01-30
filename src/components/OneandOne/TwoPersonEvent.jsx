5b7330c83ec6df4e05c4eff4e7db308e619798e1
tanjil hoassain <tanjilt46@gmail.com>
2024-01-30 08:27:11 -0700

    create event tile slot changing

--- src/components/OneandOne/TwoPersonEvent.jsx
+++ src/components/OneandOne/TwoPersonEvent.jsx
@@ -6,6 +6,13 @@ import React, { useState } from "react";
 const TwoPersonEvent = () => {
   const [next1, setNext1] = useState(false);
   const [location, setLocaion] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+  const[fromTime1,setFromTime1]=useState('')
+  const[toTime1,setToTime1]=useState('')
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [checkboxValues, setCheckboxValues] = useState(daysOfWeek.map(() => false));
+ 
 
   const formHandler = async(e) => {
     e.preventDefault();
@@ -51,6 +58,39 @@ const TwoPersonEvent = () => {
   const eHandle = (event) => {
     setLocaion(event.target.value);
   };
+
+
+
+
+
+ const selectHandler=()=>{
+   setIsChecked(!isChecked);
+
+}
+
+
+
+
+const handleCheckboxChange = (index) => {
+  const newCheckboxValues = [...checkboxValues];
+  newCheckboxValues[index] = !newCheckboxValues[index];
+  setCheckboxValues(newCheckboxValues);
+  setSelectedDay(newCheckboxValues[index] ? index : null);
+ 
+
+  console.log('checkboc clickked',fromTime1,toTime1,selectedDay)
+};
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <div className="my-10">
@@ -145,129 +185,197 @@ const TwoPersonEvent = () => {
           </label>
           <p className="text-sm">Set your availability during the week.</p>
 
-          <div className="flex gap-3">
-            <div className="space-y-4">
-              {/* slect bar */}
 
-              <select
-                className="select select-bordered select-xs w-[100px] my-3 max-w-xs"
-                name="days"
-                defaultValue="default"
-              >
-                <option disabled value="default" selected>
-                  Select Day
-                </option>
-                <option value="saturday">Saturday</option>
-                <option value="sunday">Sunday</option>
-                <option value="monday">Monday</option>
-                <option value="tuesday">Tuesday</option>
-                <option value="wednesday">Wednesday</option>
-                <option value="thursday">Thursday</option>
-                <option value="friday">Friday</option>
-              </select>
-            </div>
 
-            <div className="">
-              {/* avaiable from */}
+           <div className="">
+            {/* first input implement
+           <div className="form-control flex flex-row items-center">
+ 
+    
+  <input  onChange={selectHandler} checked={isChecked} type="checkbox"  className="checkbox checkbox-xs" /> 
+  <label className="label">
+    <span className="label-text">Saturday</span> 
 
-              <select
-                className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
-                name="fromTime"
-                defaultValue="default"
-              >
-                <option disabled value="default" selected>
-                  From
-                </option>
-                <option value="6:00 AM">6:00 AM</option>
-                <option value="6:00 AM">6:30 AM</option>
-                <option value="7:00 AM">7:00 AM</option>
-                <option value="7:30 AM">7:30 AM</option>
-                <option value="8:00 AM">8:00 AM</option>
-                <option value="8:30 AM">8:30 AM</option>
-                <option value="9:00 AM">9:00 AM</option>
-                <option value="9:30 AM">9:30 AM</option>
-                <option value="10:00 AM">10:00 AM</option>
-                <option value="10:30 AM">10:30 AM</option>
-                <option value="11:00 AM">11:00 AM</option>
-                <option value="11:30 AM">11:30 AM</option>
-                <option value="12:00 PM">12:00 PM</option>
-                <option value="12:30 PM">12:30 PM</option>
-                <option value="1:00 PM">1:00 PM</option>
-                <option value="1:30 PM">1:30 PM</option>
-                <option value="2:00 PM">2:00 PM</option>
-                <option value="2:30 PM">2:30 PM</option>
-                <option value="3:00 PM">3:00 PM</option>
-                <option value="3:30 PM">3:30 PM</option>
-                <option value="4:00 PM">4:00 PM</option>
-                <option value="4:30 PM">4:30 PM</option>
-                <option value="5:00 PM">5:00 PM</option>
-                <option value="5:30 PM">5:30 PM</option>
-                <option value="6:00 PM">6:00 PM</option>
-                <option value="6:30 PM">6:30 PM</option>
-                <option value="7:00 PM">7:00 PM</option>
-                <option value="7:30 PM">7:30 PM</option>
-                <option value="8:00 PM">8:00 PM</option>
-                <option value="8:30 PM">8:30 PM</option>
-                <option value="9:00 PM">9:00 PM</option>
-                <option value="9:30 PM">9:30 PM</option>
-                <option value="10:00 PM">10:00 PM</option>
-                <option value="10:30 PM">10:30 PM</option>
-                <option value="11:00 PM">11:00 PM</option>
-                <option value="11:30 PM">11:30 PM</option>
-                <option value="12:00 AM">12:00 AM</option>
-              </select>
-            </div>
+    <select
+     className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
+     name="fromTime"
+     disabled={!isChecked}
+     onChange={(event)=>setFromTime1(event.target.value)}
+    defaultValue="default"
+  >    <option disabled value="default" selected>      From
+    </option>
+     <option value="6:00 AM">6:00 AM</option>
+    <option value="6:00 AM">6:30 AM</option>
+     <option value="7:00 AM">7:00 AM</option>
+    <option value="7:30 AM">7:30 AM</option>
+     <option value="8:00 AM">8:00 AM</option>
+     </select>
 
-            <div className="">
-              {/* avaiale to */}
-              <select
-                className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
-                name="toTime"
-                defaultValue="default"
-              >
-                <option disabled value="default" selected>
-                  To
-                </option>
-                <option value="6:00 AM">6:00 AM</option>
-                <option value="6:00 AM">6:30 AM</option>
-                <option value="7:00 AM">7:00 AM</option>
-                <option value="7:30 AM">7:30 AM</option>
-                <option value="8:00 AM">8:00 AM</option>
-                <option value="8:30 AM">8:30 AM</option>
-                <option value="9:00 AM">9:00 AM</option>
-                <option value="9:30 AM">9:30 AM</option>
-                <option value="10:00 AM">10:00 AM</option>
-                <option value="10:30 AM">10:30 AM</option>
-                <option value="11:00 AM">11:00 AM</option>
-                <option value="11:30 AM">11:30 AM</option>
-                <option value="12:00 PM">12:00 PM</option>
-                <option value="12:30 PM">12:30 PM</option>
-                <option value="1:00 PM">1:00 PM</option>
-                <option value="1:30 PM">1:30 PM</option>
-                <option value="2:00 PM">2:00 PM</option>
-                <option value="2:30 PM">2:30 PM</option>
-                <option value="3:00 PM">3:00 PM</option>
-                <option value="3:30 PM">3:30 PM</option>
-                <option value="4:00 PM">4:00 PM</option>
-                <option value="4:30 PM">4:30 PM</option>
-                <option value="5:00 PM">5:00 PM</option>
-                <option value="5:30 PM">5:30 PM</option>
-                <option value="6:00 PM">6:00 PM</option>
-                <option value="6:30 PM">6:30 PM</option>
-                <option value="7:00 PM">7:00 PM</option>
-                <option value="7:30 PM">7:30 PM</option>
-                <option value="8:00 PM">8:00 PM</option>
-                <option value="8:30 PM">8:30 PM</option>
-                <option value="9:00 PM">9:00 PM</option>
-                <option value="9:30 PM">9:30 PM</option>
-                <option value="10:00 PM">10:00 PM</option>
-                <option value="10:30 PM">10:30 PM</option>
-                <option value="11:00 PM">11:00 PM</option>
-                <option value="11:30 PM">11:30 PM</option>
-                <option value="12:00 AM">12:00 AM</option>
-              </select>
-            </div>
+
+     <select
+     className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
+     name="fromTime"
+     value={toTime1}
+     disabled={!isChecked}
+     onChange={(event)=>setToTime1(event.target.value)}
+    defaultValue="default"
+  >    <option disabled value="default" selected>      To
+    </option>
+     <option value="6:00 AM">6:00 AM</option>
+    <option value="6:00 AM">6:30 AM</option>
+     <option value="7:00 AM">7:00 AM</option>
+    <option value="7:30 AM">7:30 AM</option>
+     <option value="8:00 AM">8:00 AM</option>
+     </select>
+
+  </label>
+</div> */}
+
+
+
+
+
+{/* first tme slots */}
+
+<div className=" ">
+{daysOfWeek.map((day,index) => (
+        <div key={index} className="flex flex-row  items-center">
+          <input  className="checkbox checkbox-xs"
+            type="checkbox"
+            checked={isChecked}
+            onChange={() => handleCheckboxChange(day)}
+          />
+          <label className="label">
+    <span className="label-text">{day}</span> 
+
+    <select
+     className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
+     name="fromTime"
+     disabled={!isChecked}
+     onChange={(event)=>setFromTime1(event.target.value)}
+    defaultValue="default"
+  >    <option disabled value="default" selected>      From
+    </option>
+    <option value="6:00 AM">6:00 AM</option>
+     <option value="6:00 AM">6:30 AM</option>
+     <option value="7:00 AM">7:00 AM</option>
+    <option value="7:30 AM">7:30 AM</option>
+    <option value="8:00 AM">8:00 AM</option>
+    <option value="8:30 AM">8:30 AM</option>
+   <option value="9:00 AM">9:00 AM</option>
+     <option value="9:30 AM">9:30 AM</option>
+     <option value="10:00 AM">10:00 AM</option>
+    <option value="10:30 AM">10:30 AM</option>
+     <option value="11:00 AM">11:00 AM</option>
+    <option value="11:30 AM">11:30 AM</option>
+     <option value="12:00 PM">12:00 PM</option>
+    <option value="12:30 PM">12:30 PM</option>
+    <option value="1:00 PM">1:00 PM</option>
+    <option value="1:30 PM">1:30 PM</option>
+    <option value="2:00 PM">2:00 PM</option>
+    <option value="2:30 PM">2:30 PM</option>
+     <option value="3:00 PM">3:00 PM</option>
+     <option value="3:30 PM">3:30 PM</option>
+    <option value="4:00 PM">4:00 PM</option>
+     <option value="4:30 PM">4:30 PM</option>
+     <option value="5:00 PM">5:00 PM</option>
+    <option value="5:30 PM">5:30 PM</option>
+    <option value="6:00 PM">6:00 PM</option>
+    <option value="6:30 PM">6:30 PM</option>
+    <option value="7:00 PM">7:00 PM</option>
+     <option value="7:30 PM">7:30 PM</option>
+     <option value="8:00 PM">8:00 PM</option>
+     <option value="8:30 PM">8:30 PM</option>
+    <option value="9:00 PM">9:00 PM</option>
+    <option value="9:30 PM">9:30 PM</option>
+     <option value="10:00 PM">10:00 PM</option>
+     <option value="10:30 PM">10:30 PM</option>
+     <option value="11:00 PM">11:00 PM</option>
+    <option value="11:30 PM">11:30 PM</option>
+     <option value="12:00 AM">12:00 AM</option>
+     </select>
+
+
+     <select
+     className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
+     name="toTime"
+     value={toTime1}
+     disabled={!isChecked}
+     onChange={(event)=>setToTime1(event.target.value)}
+    defaultValue="default"
+  >    <option disabled value="default" selected>      To
+    </option>
+    <option value="6:00 AM">6:00 AM</option>
+     <option value="6:00 AM">6:30 AM</option>
+     <option value="7:00 AM">7:00 AM</option>
+    <option value="7:30 AM">7:30 AM</option>
+    <option value="8:00 AM">8:00 AM</option>
+    <option value="8:30 AM">8:30 AM</option>
+   <option value="9:00 AM">9:00 AM</option>
+     <option value="9:30 AM">9:30 AM</option>
+     <option value="10:00 AM">10:00 AM</option>
+    <option value="10:30 AM">10:30 AM</option>
+     <option value="11:00 AM">11:00 AM</option>
+    <option value="11:30 AM">11:30 AM</option>
+     <option value="12:00 PM">12:00 PM</option>
+    <option value="12:30 PM">12:30 PM</option>
+    <option value="1:00 PM">1:00 PM</option>
+    <option value="1:30 PM">1:30 PM</option>
+    <option value="2:00 PM">2:00 PM</option>
+    <option value="2:30 PM">2:30 PM</option>
+     <option value="3:00 PM">3:00 PM</option>
+     <option value="3:30 PM">3:30 PM</option>
+    <option value="4:00 PM">4:00 PM</option>
+     <option value="4:30 PM">4:30 PM</option>
+     <option value="5:00 PM">5:00 PM</option>
+    <option value="5:30 PM">5:30 PM</option>
+    <option value="6:00 PM">6:00 PM</option>
+    <option value="6:30 PM">6:30 PM</option>
+    <option value="7:00 PM">7:00 PM</option>
+     <option value="7:30 PM">7:30 PM</option>
+     <option value="8:00 PM">8:00 PM</option>
+     <option value="8:30 PM">8:30 PM</option>
+    <option value="9:00 PM">9:00 PM</option>
+    <option value="9:30 PM">9:30 PM</option>
+     <option value="10:00 PM">10:00 PM</option>
+     <option value="10:30 PM">10:30 PM</option>
+     <option value="11:00 PM">11:00 PM</option>
+    <option value="11:30 PM">11:30 PM</option>
+     <option value="12:00 AM">12:00 AM</option>
+     </select>
+
+  </label>
+        
+        </div>
+      ))}
+
           </div>
+{/* first time ends */}
+
+
+
+
+
+
+
+
+           </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
           <div className="">
             <label className="label">
---
+++ src/components/OneandOne/eoeo.js
@@ -0,0 +1,124 @@
+
+{/* <div className="flex gap-3">
+<div className="space-y-4">
+  {/* slect bar */}
+
+//   <select
+//     className="select select-bordered select-xs w-[100px] my-3 max-w-xs"
+//     name="days"
+//     defaultValue="default"
+//   >
+//     <option disabled value="default" selected>
+//       Select Day
+//     </option>
+//     <option value="saturday">Saturday</option>
+//     <option value="sunday">Sunday</option>
+//     <option value="monday">Monday</option>
+//     <option value="tuesday">Tuesday</option>
+//     <option value="wednesday">Wednesday</option>
+//     <option value="thursday">Thursday</option>
+//     <option value="friday">Friday</option>
+//   </select>
+// </div>
+
+// <div className="">
+//   {/* avaiable from */}
+
+//   <select
+//     className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
+//     name="fromTime"
+//     defaultValue="default"
+//   >
+//     <option disabled value="default" selected>
+//       From
+//     </option>
+//     <option value="6:00 AM">6:00 AM</option>
+//     <option value="6:00 AM">6:30 AM</option>
+//     <option value="7:00 AM">7:00 AM</option>
+//     <option value="7:30 AM">7:30 AM</option>
+//     <option value="8:00 AM">8:00 AM</option>
+//     <option value="8:30 AM">8:30 AM</option>
+//     <option value="9:00 AM">9:00 AM</option>
+//     <option value="9:30 AM">9:30 AM</option>
+//     <option value="10:00 AM">10:00 AM</option>
+//     <option value="10:30 AM">10:30 AM</option>
+//     <option value="11:00 AM">11:00 AM</option>
+//     <option value="11:30 AM">11:30 AM</option>
+//     <option value="12:00 PM">12:00 PM</option>
+//     <option value="12:30 PM">12:30 PM</option>
+//     <option value="1:00 PM">1:00 PM</option>
+//     <option value="1:30 PM">1:30 PM</option>
+//     <option value="2:00 PM">2:00 PM</option>
+//     <option value="2:30 PM">2:30 PM</option>
+//     <option value="3:00 PM">3:00 PM</option>
+//     <option value="3:30 PM">3:30 PM</option>
+//     <option value="4:00 PM">4:00 PM</option>
+//     <option value="4:30 PM">4:30 PM</option>
+//     <option value="5:00 PM">5:00 PM</option>
+//     <option value="5:30 PM">5:30 PM</option>
+//     <option value="6:00 PM">6:00 PM</option>
+//     <option value="6:30 PM">6:30 PM</option>
+//     <option value="7:00 PM">7:00 PM</option>
+//     <option value="7:30 PM">7:30 PM</option>
+//     <option value="8:00 PM">8:00 PM</option>
+//     <option value="8:30 PM">8:30 PM</option>
+//     <option value="9:00 PM">9:00 PM</option>
+//     <option value="9:30 PM">9:30 PM</option>
+//     <option value="10:00 PM">10:00 PM</option>
+//     <option value="10:30 PM">10:30 PM</option>
+//     <option value="11:00 PM">11:00 PM</option>
+//     <option value="11:30 PM">11:30 PM</option>
+//     <option value="12:00 AM">12:00 AM</option>
+//   </select>
+// </div>
+
+// <div className="">
+//   {/* avaiale to */}
+//   <select
+//     className="select select-bordered select-xs w-[105px] my-3 max-w-xs"
+//     name="toTime"
+//     defaultValue="default"
+//   >
+//     <option disabled value="default" selected>
+//       To
+//     </option>
+//     <option value="6:00 AM">6:00 AM</option>
+//     <option value="6:00 AM">6:30 AM</option>
+//     <option value="7:00 AM">7:00 AM</option>
+//     <option value="7:30 AM">7:30 AM</option>
+//     <option value="8:00 AM">8:00 AM</option>
+//     <option value="8:30 AM">8:30 AM</option>
+//     <option value="9:00 AM">9:00 AM</option>
+//     <option value="9:30 AM">9:30 AM</option>
+//     <option value="10:00 AM">10:00 AM</option>
+//     <option value="10:30 AM">10:30 AM</option>
+//     <option value="11:00 AM">11:00 AM</option>
+//     <option value="11:30 AM">11:30 AM</option>
+//     <option value="12:00 PM">12:00 PM</option>
+//     <option value="12:30 PM">12:30 PM</option>
+//     <option value="1:00 PM">1:00 PM</option>
+//     <option value="1:30 PM">1:30 PM</option>
+//     <option value="2:00 PM">2:00 PM</option>
+//     <option value="2:30 PM">2:30 PM</option>
+//     <option value="3:00 PM">3:00 PM</option>
+//     <option value="3:30 PM">3:30 PM</option>
+//     <option value="4:00 PM">4:00 PM</option>
+//     <option value="4:30 PM">4:30 PM</option>
+//     <option value="5:00 PM">5:00 PM</option>
+//     <option value="5:30 PM">5:30 PM</option>
+//     <option value="6:00 PM">6:00 PM</option>
+//     <option value="6:30 PM">6:30 PM</option>
+//     <option value="7:00 PM">7:00 PM</option>
+//     <option value="7:30 PM">7:30 PM</option>
+//     <option value="8:00 PM">8:00 PM</option>
+//     <option value="8:30 PM">8:30 PM</option>
+//     <option value="9:00 PM">9:00 PM</option>
+//     <option value="9:30 PM">9:30 PM</option>
+//     <option value="10:00 PM">10:00 PM</option>
+//     <option value="10:30 PM">10:30 PM</option>
+//     <option value="11:00 PM">11:00 PM</option>
+//     <option value="11:30 PM">11:30 PM</option>
+//     <option value="12:00 AM">12:00 AM</option>
+//   </select>
+// </div>
+// </div> */}
