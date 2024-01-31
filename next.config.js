ed11c0e4094337562382064343c1caee7d64d15e
Enayet <enayetflweb@gamil.com>
2024-01-31 19:04:38 +0600

    file name change

--- next.config.js
+++ next.config.js
@@ -6,4 +6,25 @@ const nextConfig = {
     pageExtensions: ['tsx', 'ts', 'jsx', 'js']
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+// // next.config.js
+const path = require('path');
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Add aliases from tsconfig.json
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/*': path.resolve(__dirname, 'src/*'),
+    };
+
+    // For using Preact in production
+    if (!isServer) {
+      config.resolve.alias['react'] = 'preact/compat';
+      config.resolve.alias['react-dom/test-utils'] = 'preact/test-utils';
+      config.resolve.alias['react-dom'] = 'preact/compat';
+    }
+
+    return config;
+  },
+};
