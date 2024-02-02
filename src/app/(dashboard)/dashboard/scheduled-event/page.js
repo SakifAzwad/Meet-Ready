a7fed16d2c2fd2a4cdfce4106af43e4d5794d753
Enayet <enayetflweb@gamil.com>
2024-02-02 16:54:37 +0600

    scheduled-event page copied

--- src/app/(dashboard)/dashboard/scheduled-event/page.js
+++ src/app/(dashboard)/dashboard/scheduled-event/page.js
@@ -1,57 +1,119 @@
-"use client"
-import EventCard from "@/components/EventCard/EventCard";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+// "use client"
+// import EventCard from "@/components/EventCard/EventCard";
+// import { useSession } from "next-auth/react";
+// import Link from "next/link";
+// import { useEffect, useState } from "react";
+// import { FaMagnifyingGlass } from "react-icons/fa6";
 
-const ScheduledEvent = () => {
-  // Getting user email from session
-  const session = useSession()
-  const email = session?.data?.user?.email
-  const name = session?.data?.user?.name
-  console.log(name)
+// const ScheduledEvent = () => {
+//   // Getting user email from session
+//   const session = useSession()
+//   const email = session?.data?.user?.email
+//   const name = session?.data?.user?.name
+//   console.log(name)
 
-  console.log(email)
-  // State for storing event
-  const [eventData, setEventData] = useState(null)
+//   console.log(email)
+//   // State for storing event
+//   const [eventData, setEventData] = useState(null)
 
-  const getScheduledEvent = async() => {
-    console.log(email)
-    try {
-    const res = await fetch (`/api/event?email=${email}`, {
-      cache: "no-store", 
-    })
-    if (res.status === 500) {
-      console.log("An error ocurred please try again.");
-    }
+//   const getScheduledEvent = async() => {
+//     console.log(email)
+//     try {
+//     const res = await fetch (`/api/event?email=${email}`, {
+//       cache: "no-store", 
+//     })
+//     if (res.status === 500) {
+//       console.log("An error ocurred please try again.");
+//     }
 
-    const eventData = await res.json();
-    console.log(eventData)
-    setEventData(eventData?.scheduledEvent);
-    } catch (error) {
+//     const eventData = await res.json();
+//     console.log(eventData)
+//     setEventData(eventData?.scheduledEvent);
+//     } catch (error) {
       
-    }
-  }
+//     }
+//   }
 
-  useEffect(() => {
-    getScheduledEvent()
-  }, [email])
+//   useEffect(() => {
+//     getScheduledEvent()
+//   }, [email])
 
-  console.log(eventData)
+//   console.log(eventData)
 
-  return (
-    <div className="flex flex-col justify-start mt-16 space-y-5">
-      <h1 className="text-2xl font-semibold pl-2 border-l-2 border-purple-500">
-        Scheduled Events
-      </h1>
+//   return (
+//     <div className="flex flex-col justify-start mt-16 space-y-5">
+//       <h1 className="text-2xl font-semibold pl-2 border-l-2 border-purple-500">
+//         Scheduled Events
+//       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {/* <EventCard  /> */}
+//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+//         {/* <EventCard  /> */}
         
              
 
-      </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ScheduledEvent;
+
+
+import ScheduleEvents from "@/components/ScheduleEvents/ScheduleEvents";
+
+
+const ScheduledEvent = () => {
+  // Getting user email from session
+  // const session = useSession()
+  // const email = session?.data?.user?.email
+  // const name = session?.data?.user?.name
+  // console.log(name)
+
+  // console.log(email)
+  // State for storing event
+  // const [eventData, setEventData] = useState(null)
+
+  // const getScheduledEvent = async() => {
+  //   console.log(email)
+  //   try {
+  //   const res = await fetch (`/api/event?email=${email}`, {
+  //     cache: "no-store", 
+  //   })
+  //   if (res.status === 500) {
+  //     console.log("An error ocurred please try again.");
+  //   }
+
+  //   const eventData = await res.json();
+  //   console.log(eventData)
+  //   setEventData(eventData?.scheduledEvent);
+  //   } catch (error) {
+      
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getScheduledEvent()
+  // }, [getScheduledEvent])
+
+  // console.log(eventData)
+
+
+
+
+  return (
+    <div className="">
+      <h1 className="text-2xl text-center font-semibold pl-2 border-l-2 border-purple-500">
+        Scheduled Events
+      </h1>
+
+    <div className="my-8">
+    <ScheduleEvents></ScheduleEvents>
+    </div>
+
+     
+             
+
+  
     </div>
   );
 };
