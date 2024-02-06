f2fc4595f94d60efceccc123c76bdae939a296e2
Enayet <enayetflweb@gamil.com>
2024-02-06 20:27:37 +0600

    copy commit

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -27,3 +27,38 @@ const sendMail = (to, subject, text) => {
 };
 
 module.exports = { sendMail };
+
+
+
+export const POST = async (request) =>{
+  try {
+    const bookingData = await request.json();
+  
+    console.log('data from frontend',bookingData)
+    await connect();
+  
+    const newBookingData = new Event(bookingData)
+
+
+  console.log('newBookingData', newBookingData)
+   try {
+      await newBookingData.save()
+
+      // Send email to the original user who created the event
+      const userEmail = bookingData.userEmail; // Change this based on your data structure
+      const subject = 'Booking Information Received';
+      const text = 'Thank you for receiving booking information.';
+
+      // Call the sendMail function
+      sendMail(userEmail, subject, text);
+
+      return new NextResponse("Booking successfully made.", {status: 200})
+    } catch (error) {
+      console.log(error)
+      return new NextResponse(error, {status: 500})
+    }
+  } catch (error) {
+    console.log('Internal server error', error)
+    return new NextResponse("Internal Server Error", {status: 500})
+  }
+  }
