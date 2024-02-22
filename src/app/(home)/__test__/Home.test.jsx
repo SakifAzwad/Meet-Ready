7f068372b150a553e3724e763ce0d088453c899f
Enayet <enayetflweb@gamil.com>
2024-02-22 14:19:56 +0600

    conflict commit

--- src/app/(home)/__test__/Home.test.jsx
+++ src/app/(home)/__test__/Home.test.jsx
@@ -1,47 +1,47 @@
-// // File: Home.test.js
+// File: Home.test.js
 
-// import { render, screen } from '@testing-library/react';
-// import Home from '@/app/(home)/page';
-// import './mockResizeObserver'
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/(home)/page';
+import './mockResizeObserver'
 
 
-// describe('Home page', () => {
-//   it('renders Navbar component', () => {
-//     render(<Home/>)
+describe('Home page', () => {
+  it('renders Navbar component', () => {
+    render(<Home/>)
 
-//     const navbar = screen.getByTestId('homeNavbar')
+    const navbar = screen.getByTestId('homeNavbar')
 
-//     expect(navbar).toBeInTheDocument()
-//   });
+    expect(navbar).toBeInTheDocument()
+  });
 
-//   it('renders Banner1 component', () => {
-//     render(<Home/>)
+  it('renders Banner1 component', () => {
+    render(<Home/>)
 
-//     const banner1 = screen.getByTestId('homeBanner')
+    const banner1 = screen.getByTestId('homeBanner')
 
-//     expect(banner1).toBeInTheDocument()
-//   });
-//   it('renders Banner 2 component', () => {
-//     render(<Home/>)
+    expect(banner1).toBeInTheDocument()
+  });
+  it('renders Banner 2 component', () => {
+    render(<Home/>)
 
-//     const banner2 = screen.getByTestId('homeBanner2')
+    const banner2 = screen.getByTestId('homeBanner2')
 
-//     expect(banner2).toBeInTheDocument()
-//   });
-//   it('renders Banner 3 component', () => {
-//     render(<Home/>)
+    expect(banner2).toBeInTheDocument()
+  });
+  it('renders Banner 3 component', () => {
+    render(<Home/>)
 
-//     const banner3 = screen.getByTestId('homeBanner3')
+    const banner3 = screen.getByTestId('homeBanner3')
 
-//     expect(banner3).toBeInTheDocument()
-//   });
-//   it('renders footer component', () => {
-//     render(<Home/>)
+    expect(banner3).toBeInTheDocument()
+  });
+  it('renders footer component', () => {
+    render(<Home/>)
 
-//     const footer = screen.getByTestId('homeFooter')
+    const footer = screen.getByTestId('homeFooter')
 
-//     expect(footer).toBeInTheDocument()
-//   });
+    expect(footer).toBeInTheDocument()
+  });
 
   
-// });
+});
--- src/middleware.js
+++ src/middleware.js
@@ -10,10 +10,10 @@ export function middleware(request) {
     request.cookies.get("__Secure-next-auth.session-token")?.value || "";
 
   // TODO active this when using local host
-  // const token = request.cookies.get('next-auth.session-token')?.value || ''
-// console.log('token', token)
-  if(isPublicPath && token){
-    return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
+  // const token = request.cookies.get("next-auth.session-token")?.value || "";
+  console.log("token", token);
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
   }
 
   if (!isPublicPath && !token) {
