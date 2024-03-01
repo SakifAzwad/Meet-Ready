17a4291cf8f33d00a5ca48e7cc125aa2d8689b29
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2024-03-01 20:32:12 +0600

    updated layout

--- src/app/(JobSeeker)/layout.jsx
+++ src/app/(JobSeeker)/layout.jsx
@@ -20,8 +20,8 @@ const JobSeekerDashboardLayout = async ({ children }) => {
     redirect('/roleSetting')
   }
   return (
-    <div className="">
-      <div className="max-w-7xl mx-auto min-h-screen">{ children }</div>
+    <div className="max-w-7xl mx-auto min-h-screen">
+      {children}
     </div>
   )
 }
